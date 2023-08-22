532e853a264df8df6963eed6b8ca7b3e4845d2d9
carl <jiale.liu@coolkit.cn>
2023-08-22 16:56:25 +0800

    feat: Add the process of refreshing tokens

--- controlDeviceDemo.js
+++ controlDeviceDemo.js
@@ -8,17 +8,53 @@ const controlDeviceDemo = async () => {
   }
 
   // get token
-  const LoggedInfo = JSON.parse(fs.readFileSync('./token.json', 'utf-8'))
+  let LoggedInfo = JSON.parse(fs.readFileSync('./token.json', 'utf-8'))
   // console.info(LoggedInfo)
-
-  // client.syncLocalToken(region, account= "");
   client.at = LoggedInfo.data?.accessToken
   client.region = LoggedInfo?.region || 'eu'
   client.setUrl(LoggedInfo?.region || 'eu')
+  // Check if the token has expired, and refresh the token if it has expired
+  if (
+    LoggedInfo.data?.atExpiredTime < Date.now() &&
+    LoggedInfo.data?.rtExpiredTime > Date.now()
+  ) {
+    console.log('Token expired, refreshing token')
+    const refreshStatus = await client.user.refreshToken({
+      rt: LoggedInfo.data?.refreshToken,
+    })
+    console.log(refreshStatus)
+    if (refreshStatus.error === 0) {
+      // You can also use built-in storage
+      // client.storage.set('token', {...})
+      fs.writeFileSync(
+        './token.json',
+        JSON.stringify({
+          status: 200,
+          responseTime: 0,
+          error: 0,
+          msg: '',
+          data: {
+            accessToken: refreshStatus?.data?.at,
+            atExpiredTime: Date.now() + 2592000000,
+            refreshToken: refreshStatus?.data?.rt,
+            rtExpiredTime: Date.now() + 5184000000,
+          },
+          region: client.region,
+        })
+      )
+      LoggedInfo = JSON.parse(fs.readFileSync('./token.json', 'utf-8'))
+    }
+  }
+
+  if (LoggedInfo.data?.rtExpiredTime < Date.now()) {
+    console.log('Failed to refresh token, need to log in again to obtain token')
+    return
+  }
 
   // Get device list
   try {
-    let thingList = await client.device.getAllThings({}) // { status: 200, error: 0, msg: "", data: { thingList: [{ itemType: 1, itemData: {...} }], total: 1 } };
+    let thingList = await client.device.getAllThingsAllPages({}) // { status: 200, error: 0, msg: "", data: { thingList: [{ itemType: 1, itemData: {...} }], total: 1 } };
+    console.log(JSON.stringify(thingList, null, 2))
     if (thingList?.error === 0) {
       thingList = thingList.data.thingList
       console.log(thingList)
